d6d4db9800eeb72015041f76fcff63b7d7fc92ba
benib <bb@xiala.net>
2015-08-15 18:37:21 +0200

    update to aurelia-leaflet@0.1.1

--- jspm_packages/github/benib/aurelia-leaflet@0.1.0.js
+++
@@ -1,3 +0,0 @@
-define(["github:benib/aurelia-leaflet@0.1.0/index"], function(main) {
-  return main;
-});
---
+++ jspm_packages/github/benib/aurelia-leaflet@0.1.1.js
@@ -0,0 +1,3 @@
+define(["github:benib/aurelia-leaflet@0.1.1/index"], function(main) {
+  return main;
+});
