d04d802607f7fd4c8387060250aa7799a4f8d479
Beni <beni.buess@nzz.ch>
2017-01-14 15:07:56 +0100

    fixes leaflet default images path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Leaflet from 'leaflet';
 
 export function configure(frameworkConfig, {
-    LeafletDefaultImagePath = 'jspm_packages/npm/leaflet@1.0.2/dist/images'
+    LeafletDefaultImagePath = 'jspm_packages/npm/leaflet@1.0.2/dist/images/'
   } = {}) {
     Leaflet.Icon.Default.imagePath = LeafletDefaultImagePath;
     frameworkConfig.globalResources('./leaflet');
